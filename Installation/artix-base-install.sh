0c9c26c0808f8bcf3634bc6cbd4877902b8b8c84
andradeexperiments <phiandrade@proton.me>
2022-11-17 18:00:55 -0300

    17/11/2022

--- Installation/artix-base-install.sh
+++ Installation/artix-base-install.sh
@@ -5,8 +5,7 @@ ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 
 # Locale
-locale -a
-sed -i '178s/.//' /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.conf
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
