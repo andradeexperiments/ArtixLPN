67cf66bf727201cebe533d4ad5b231a8d012fddf
andradeexperiments <phiandrade@proton.me>
2022-11-17 11:19:10 -0300

    17/11/2022

---
+++ Installation/move-files.sh
@@ -0,0 +1,12 @@
+# Criando diretórios e movendo arquivos
+mkdir ~/Others
+mv ~/ArtixLPN/Scripts ~/Others/
+mv ~/ArtixLPN/seahorse.txt ~/Others/
+mv ~/ArtixLPN/Dotfiles/.config ~/
+rm -rf ~/.bashrc
+mv ~/ArtixLPN/Dotfiles/.bashrc ~/
+mv ~/ArtixLPN/Dotfiles/.zshrc ~/
+mv ~/ArtixLPN/Dotfiles/.dwm ~/
+mv ~/ArtixLPN/Dotfiles/.xinitrc ~/
+
+printf "\e[1;32mFinalizado! Aproveite seu sistema.\e[0m"
